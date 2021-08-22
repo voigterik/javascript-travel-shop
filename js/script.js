69ea7acecb05151357c4ac2feec0aa9e8c0b93e1
Your Name <you@example.com>
2021-08-22 11:16:08 +0200

    header search and temp destination image

--- js/script.js
+++ js/script.js
@@ -58,6 +58,42 @@ const toggleMenu = (button) => {
 }
 menuButton.addEventListener("click", toggleMenu);
 
+// toggle search input in header
+const toggleSearch = () => {
+    const searchContainer = document.querySelector(".js-search");
+    const searchIcon = document.querySelector(".js-search-icon");
+    const searchIconClose = document.querySelector(".js-close-search-icon");
+    const searchInput = document.querySelector(".js-search-input");
+
+    searchIcon.addEventListener("click", () => {
+        searchContainer.classList.add("m-active");
+        searchInput.classList.add("m-active");
+    });
+
+    searchIconClose.addEventListener("click", () => {
+        searchContainer.classList.remove("m-active");
+        searchInput.classList.remove("m-active");
+    });
+
+}
+toggleSearch();
+
+// search for destinations
+const headerSearch = (searchTerm) => {
+    const destinations = document.querySelectorAll(".js-marker");
+    destinations.forEach(destination => {
+        if(destination.innerText.toLowerCase().includes(searchTerm.toLowerCase())){
+            destination.parentNode.classList.remove("m-hide");
+            
+        } else 
+            destination.parentNode.classList.add("m-hide");
+    });
+}
+const searchInput = document.querySelector(".js-search-input");
+searchInput.addEventListener("input", (e) => {
+    headerSearch(e.target.value);
+});
+
 // get list of countries from API
 async function getCountries(){
     const response = await fetch("https://restcountries.eu/rest/v2/all");
@@ -80,10 +116,11 @@ async function getCountries(){
 
         // create card HTML
         const card = document.createElement("div");
-        card.classList.add(`js-region-${country.region}`, "js-card", "b-card");
+        card.classList.add(`js-region-${country.region.toLowerCase()}`, "js-card", "b-card");
 
         // card HTML
         card.innerHTML = `
+            <div class="js-marker">${country.region.toLowerCase()} ${country.name.toLowerCase()}</div>
             <span class="js-badge e-badge"></span>
             <div class="js-hover-container e-hover-container"></div>
             <div class="js-extra e-extra">
@@ -94,15 +131,15 @@ async function getCountries(){
                     <i class="fa fa-thumbs-up"></i> <span class="js-percent e-percent">79%</span>
                 </span>
             </div>
-            <div class="js-image js-country-${country.name} e-image">
+            <div class="js-image js-country-${country.name.toLowerCase()} e-image">
                 <img 
-                    src="https://images.unsplash.com/photo-1562620948-7ef06527f430?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=80" 
+                    src="https://images.unsplash.com/photo-1629477091314-7196cc5395f8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=376&q=80" 
                     alt="" 
                     class="js-image"                    
                 />
             </div>
             <div class="js-more-info e-more-info">
-                <h5 class="js-country-name">${country.name}</h5>
+                <h5 class="js-name">${country.name}</h5>
                 <div class="e-info-table">
                     <span class="m-bold">Capital:</span> <span>${country.capital}</span>
                     <span class="m-bold">Currency:</span> <span>${currency.join(", ")}</span>
@@ -269,22 +306,28 @@ const cardNumbers = () => {
 function showBrokenImage() {
     const images = document.querySelectorAll(".js-image img");
     images.forEach(image => {
-        console.log(image.offsetHeight);
         if(image.offsetHeight === 0){
             image.parentNode.classList.add("m-broken-image");
         } 
     });
 }
 
+function countryNames () {
+    const names = document.querySelectorAll("[class*=js-country-]");
+    names.forEach(name => {
+        if(name.classList.contains("js-country-afghanistan")){
+            const image = name.querySelector(".js-image");
+            image.src = Object.values(afghanistan);
+        }
+    });
+}
+setTimeout(() => {
+    countryNames();
+}, 5000);
 
-
-const photos = [
-    {
-        afghanistan: "https://images.unsplash.com/photo-1562620948-7ef06527f430?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=80"
-    },
-    {
-        albania: "https://images.unsplash.com/photo-1562620948-7ef06527f430?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=80"
-    }
-];
+// TEST FOR IMAGES
+const afghanistan = {
+    src: "https://images.unsplash.com/photo-1583376334098-e8816bf6f8da?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=80"
+}
 
 
